eb1d5f80433770e58dff6606ce01300425472727
DESKTOP-M2RTCJ7\Metanet <kimook73@naver.com>
2023-11-12 13:05:33 +0900

    초기 커밋.

---
+++ WpfApp7/Duty.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.ObjectModel;
+
+namespace WpfApp7
+{
+    public enum DutyType
+    {
+        Inner,
+        OutSide
+    }
+
+    public class Duty
+    {
+        private string _name;   //직무명   
+        private DutyType _dutyType; //직무타입
+
+        public Duty() { }
+        public Duty(string name, DutyType dutyType)
+        {
+            _name = name;
+            _dutyType = dutyType;   
+        }
+
+        public string DutyName
+        {
+            get { return _name; }  
+            set { _name = value; }  
+        }
+
+        public DutyType DutyType { 
+            get { return _dutyType; }
+            set { _dutyType = value; }
+        }
+
+    }
+
+    public class Duties : ObservableCollection<Duty>
+    {
+        public Duties()
+        { 
+            Add(new Duty("SALES",DutyType.OutSide));
+            Add(new Duty("LOGISTIC", DutyType.OutSide));
+            Add(new Duty("IT", DutyType.Inner));
+            Add(new Duty("MARKETING", DutyType.Inner));
+            Add(new Duty("HR", DutyType.Inner));
+            Add(new Duty("PROPOTION", DutyType.OutSide));
+
+
+        } 
+    }
+}
---
+++ WpfApp7/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿
+using System.ComponentModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace WpfApp7
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        internal static Duties duties = new Duties(); 
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnSelected(object sender, RoutedEventArgs e)
+        {
+            if((sender as ListBox).SelectedItem !=null) 
+            {
+                string dutyType = ((sender as ListBox).SelectedItem as ListBoxItem).Content.ToString();
+
+                DataContext = from duty in duties
+                              where duty.DutyType.ToString() == dutyType
+                              select duty;
+
+            }
+        }  
+
+        private void OnSelected2(object sender, RoutedEventArgs e)
+        {
+            var duty = (Duty)myListBox2.SelectedItem;
+            //string value = duty == null ? "No Selection" : duty.ToString();
+
+            MessageBox.Show(duty.DutyName + "::" + duty.DutyType, "선택한직무");
+
+        }
+        
+        //직무추가 버튼을 클릭했을 때 새창 띄움
+        private void OpenNewWindow(object sender, RoutedEventArgs e)
+        {
+            SubWindow subWindow = new SubWindow();
+
+            RefreshListEvent += new RefreshList(RefreshListBox);
+            subWindow.UpdateActor = RefreshListEvent;
+            subWindow.Show();   
+        }
+
+        //아래쪽 ListBox를 Refresh하기 위한 델리게이트 및 이벤트
+        public delegate void RefreshList(DutyType dutyType);
+        public event RefreshList RefreshListEvent;  
+
+        private void RefreshListBox(DutyType dutyType)
+        {
+            myListBox1.SelectedItem = null;
+            myListBox1.SelectedIndex = (dutyType == DutyType.Inner) ? 0 : 1;
+        }
+             
+
+    }
+}
